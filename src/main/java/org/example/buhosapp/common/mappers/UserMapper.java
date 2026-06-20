c5a1852e48de5c9e0f23915a65f872b3c8a58c10
Elias8420 <eliaszelaya747@gmail.com>
2026-06-20 17:18:55 -0600

    refactor: mapper from create request to entity to give the role entity

--- src/main/java/org/example/buhosapp/common/mappers/UserMapper.java
+++ src/main/java/org/example/buhosapp/common/mappers/UserMapper.java
@@ -2,17 +2,21 @@ package org.example.buhosapp.common.mappers;
 
 import org.example.buhosapp.domain.dtos.request.user.CreateUserRequest;
 import org.example.buhosapp.domain.dtos.response.user.UserResponse;
+import org.example.buhosapp.domain.entities.Role;
 import org.example.buhosapp.domain.entities.User;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class UserMapper {
-    public User toEntityCreate(CreateUserRequest createUserRequest) {
+    public User toEntityCreate(CreateUserRequest createUserRequest, Role role) {
         return User.builder()
                 .username(createUserRequest.getUsername())
                 .card(createUserRequest.getCard())
                 .email(createUserRequest.getEmail())
                 .password(createUserRequest.getPassword())
+                .role(role)
                 .build();
     }
 
